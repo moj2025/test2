17bb2a0c57e8c13e74f06244ee155a0e0d1e5f36
hshe <hshe@163.com>
2025-09-09 14:04:19 +0800

    feat proto mod

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-pub mod api_test2 {
-    pub mod common {
-        pub mod query {
-            tonic::include_proto!("cn.pecs.api.common.query");
-        }
-        tonic::include_proto!("cn.pecs.api.common.heartbeat");
-    }
+pub mod api_proto {
+    tonic::include_proto!("cn.pecs.api");
+    tonic::include_proto!("cn.pecs.api.common");
+    pub use test2::*;
     pub mod test2 {
+        pub use oauth::*;
         pub mod oauth {
             tonic::include_proto!("cn.pecs.api.test2.oauth");
         }
@@ -14,6 +12,7 @@ pub mod api_test2 {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // api_test2::oauth_service_server
     println!("Hello, world!");
     Ok(())
 }
